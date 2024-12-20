17e13d20f4d214e4928f3276a3d45b62cade24dd
Daniel <mail@dsgriffin.com>
2024-12-20 16:32:23 +0000

    Rewrite MAD to provide safe calls

--- src/mad.rs
+++ src/mad.rs
@@ -1,45 +1,138 @@
 use ::freefare_sys;
 
-pub fn new(version: u8) -> freefare_sys::Mad {
-    unsafe { freefare_sys::mad_new(version) }
+pub struct Mad {
+    inner: freefare_sys::Mad,
 }
 
-pub fn read(tag: freefare_sys::FreefareTag) -> freefare_sys::Mad {
-    unsafe { freefare_sys::mad_read(tag) }
+impl Mad {
+    /// Creates a new Mad instance from a version
+    pub fn new(version: u8) -> Result<Self, String> {
+        let raw_mad = unsafe { freefare_sys::mad_new(version) };
+
+        if raw_mad.is_null() {
+            Err(format!("Failed to create Mad instance for version: {}", version))
+        } else {
+            Ok(Self { inner: raw_mad })
+        }
+    }
+
+    /// Reads a MAD from the given tag
+    pub fn read(tag: freefare_sys::FreefareTag) -> Result<Self, String> {
+        if tag.is_null() {
+            return Err("Tag is null. Cannot read MAD.".to_string());
+        }
+
+        let raw_mad = unsafe { freefare_sys::mad_read(tag) };
+
+        if raw_mad.is_null() {
+            Err("Failed to read MAD from the tag.".to_string())
+        } else {
+            Ok(Self { inner: raw_mad })
+        }
+    }
+
+    /// Writes a MAD to the given tag using the specified keys
+    pub fn write(
+        &self,
+        tag: freefare_sys::FreefareTag,
+        key_b_sector_00: *const freefare_sys::MifareClassicKey,
+        key_b_sector_10: *const freefare_sys::MifareClassicKey,
+    ) -> Result<(), String> {
+        if tag.is_null() {
+            return Err("Tag is null. Cannot write MAD.".to_string());
+        }
+
+        let result = unsafe { freefare_sys::mad_write(tag, self.inner, key_b_sector_00, key_b_sector_10) };
+
+        if result < 0 {
+            Err(format!("Failed to write MAD to the tag. Error code: {}", result))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Gets the MAD version
+    pub fn get_version(&self) -> Result<i32, String> {
+        let result = unsafe { freefare_sys::mad_get_version(self.inner) };
+
+        if result < 0 {
+            Err(format!("Failed to get MAD version. Error code: {}", result))
+        } else {
+            Ok(result)
+        }
+    }
+
+    /// Sets the MAD version
+    pub fn set_version(&mut self, version: u8) -> Result<(), String> {
+        unsafe { freefare_sys::mad_set_version(self.inner, version) };
+
+        Ok(())
+    }
+
+    /// Gets the card publisher sector
+    pub fn get_card_publisher_sector(&self) -> Result<freefare_sys::MifareClassicSectorNumber, String> {
+        let result = unsafe { freefare_sys::mad_get_card_publisher_sector(self.inner) };
+
+        Ok(result)
+    }
+
+    /// Sets the card publisher sector
+    pub fn set_card_publisher_sector(
+        &mut self,
+        cps: freefare_sys::MifareClassicSectorNumber,
+    ) -> Result<(), String> {
+        let result = unsafe { freefare_sys::mad_set_card_publisher_sector(self.inner, cps) };
+
+        if result < 0 {
+            Err(format!("Failed to set card publisher sector. Error code: {}", result))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Gets an AID from a sector
+    pub fn get_aid(
+        &self,
+        sector: freefare_sys::MifareClassicSectorNumber,
+        aid: &mut freefare_sys::MadAid,
+    ) -> Result<(), String> {
+        let result = unsafe { freefare_sys::mad_get_aid(self.inner, sector, aid) };
+
+        if result < 0 {
+            Err(format!("Failed to get AID. Error code: {}", result))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Sets an AID for a sector
+    pub fn set_aid(
+        &mut self,
+        sector: freefare_sys::MifareClassicSectorNumber,
+        aid: freefare_sys::MadAid,
+    ) -> Result<(), String> {
+        let result = unsafe { freefare_sys::mad_set_aid(self.inner, sector, aid) };
+
+        if result < 0 {
+            Err(format!("Failed to set AID. Error code: {}", result))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Checks if a sector is reserved
+    pub fn sector_reserved(sector: freefare_sys::MifareClassicSectorNumber) -> bool {
+        let result = unsafe { freefare_sys::mad_sector_reserved(sector) };
+
+        result != 0
+    }
 }
 
-pub fn write(tag: freefare_sys::FreefareTag, mad: freefare_sys::Mad, key_b_sector_00: freefare_sys::MifareClassicKey, key_b_sector_10: freefare_sys::MifareClassicKey) -> ::std::os::raw::c_int {
-    unsafe { freefare_sys::mad_write(tag, mad, key_b_sector_00, key_b_sector_10) }
-}
-
-pub fn get_version(mad: freefare_sys::Mad) -> ::std::os::raw::c_int {
-    unsafe { freefare_sys::mad_get_version(mad) }
-}
-
-pub fn set_version(mad: freefare_sys::Mad, version: u8) {
-    unsafe { freefare_sys::mad_set_version(mad, version) }
-}
-
-pub fn get_card_publisher_sector(mad: freefare_sys::Mad) -> freefare_sys::MifareClassicSectorNumber {
-    unsafe { freefare_sys::mad_get_card_publisher_sector(mad) }
-}
-
-pub fn set_card_publisher_sector(mad: freefare_sys::Mad, cps: freefare_sys::MifareClassicSectorNumber) -> ::std::os::raw::c_int {
-    unsafe { freefare_sys::mad_set_card_publisher_sector(mad, cps) }
-}
-
-pub fn get_aid(mad: freefare_sys::Mad, sector: freefare_sys::MifareClassicSectorNumber, aid: *mut freefare_sys::MadAid) -> ::std::os::raw::c_int {
-    unsafe { freefare_sys::mad_get_aid(mad, sector, aid) }
-}
-
-pub fn set_aid(mad: freefare_sys::Mad, sector: freefare_sys::MifareClassicSectorNumber, aid: freefare_sys::MadAid) -> ::std::os::raw::c_int {
-    unsafe { freefare_sys::mad_set_aid(mad, sector, aid) }
-}
-
-pub fn sector_reserved(sector: freefare_sys::MifareClassicSectorNumber) -> u8 {
-    unsafe { freefare_sys::mad_sector_reserved(sector) }
-}
-
-pub fn free(mad: freefare_sys::Mad) {
-    unsafe { freefare_sys::mad_free(mad) }
+impl Drop for Mad {
+    /// Ensures Mad resources are freed automatically
+    fn drop(&mut self) {
+        unsafe {
+            freefare_sys::mad_free(self.inner);
+        }
+    }
 }
